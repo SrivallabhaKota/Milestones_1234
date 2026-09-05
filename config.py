96c2f2b8f64080abdb39d3f73f2025699f930a6f
SrivallabhaKota <kotasrivallabha@gmail.com>
2026-09-05 22:09:53 +0300

    Format environment variable retrieval for TiDB

--- config.py
+++ config.py
@@ -11,14 +11,14 @@ class Config:
         # ---------------------------------------------------------
         # TiDB Cloud Configuration (Environment variables with defaults)
         # ---------------------------------------------------------
-        MYSQL_HOST     = os.environ.get('MYSQL_HOST',     'gateway01.us-west-2.prod.aws.tidbcloud.com')
-        MYSQL_PORT     = int(os.environ.get('MYSQL_PORT', 4000))
-        MYSQL_USER     = os.environ.get('MYSQL_USER',     '2J7P9todcFuFhcR.root')
-        MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'ONincsXT0DJjUfl')
-        MYSQL_DB       = os.environ.get('MYSQL_DB',       'test')
+        MYSQL_HOST     = os.environ.get('MYSQL_HOST','gateway01.us-west-2.prod.aws.tidbcloud.com')
+        MYSQL_PORT     = int(os.environ.get('MYSQL_PORT',4000))
+        MYSQL_USER     = os.environ.get('MYSQL_USER','2J7P9todcFuFhcR.root')
+        MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD','ONincsXT0DJjUfl')
+        MYSQL_DB       = os.environ.get('MYSQL_DB','test')
         
         # SSL is mandatory for TiDB Cloud
-        MYSQL_SSL      = os.environ.get('MYSQL_SSL',      'true').lower() == 'true'
+        MYSQL_SSL      = os.environ.get('MYSQL_SSL','true').lower() == 'true'
     else:
         # ---------------------------------------------------------
         # Local Database Configuration 
